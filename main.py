0f4ea4e3cbc6162a358757b16e7a7da8216128ab
akpanther <45427189+akpanther@users.noreply.github.com>
2021-12-27 19:13:05 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas;
+import xlsxwriter;
 import openpyxl;
 lmao = 'Random1'
 print("Welcome! This is the college database!")
@@ -10,12 +11,11 @@ print(college1)
 dbx = pandas.read_excel("Colleges SAT GT 1300.xlsx", sheet_name=0)
 dbr = dbx.set_index("INSTNM")
 #Filter
-cllg_namedb = dbr.loc["University of Alabama in Huntsville", :]
+cllg_namedb = dbr.loc[college1, :]
 
-#dbf = cllg_namedb.loc[{"UNITID","INSTNM", "CITY", "CONTROL", "CCBASIC", "CCUGPROF", "ADM_RATE_ALL", "SATVR75", "SATMT75", "SATMTMID", "SAT_AVG_ALL", "UGDS", "UGDS_WHITE", "UGDS_ASIAN", "TUITIONFEE_IN", "TUITIONFEE_OUT"}]
+cllg_namedb = cllg_namedb.loc[{"UNITID", "CITY", "CONTROL", "CCBASIC", "CCUGPROF", "ADM_RATE_ALL", "SATVR75", "SATMT75", "SATMTMID", "SAT_AVG_ALL", "UGDS", "UGDS_WHITE", "UGDS_ASIAN", "TUITIONFEE_IN", "TUITIONFEE_OUT"}]
 
-#print(dbf)
-
+cllg_namedb.to_excel("cllg.xlsx")
 
 
 
