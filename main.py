29a0979ebb1c4f0aac441a69c4f140d603943c7d
akpanther <45427189+akpanther@users.noreply.github.com>
2021-12-27 19:58:49 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -15,7 +15,18 @@ cllg_namedb = dbr.loc[college1, :]
 
 cllg_namedb = cllg_namedb.loc[{"UNITID", "CITY", "CONTROL", "CCBASIC", "CCUGPROF", "ADM_RATE_ALL", "SATVR75", "SATMT75", "SATMTMID", "SAT_AVG_ALL", "UGDS", "UGDS_WHITE", "UGDS_ASIAN", "TUITIONFEE_IN", "TUITIONFEE_OUT"}]
 
-cllg_namedb.to_excel("cllg.xlsx")
+cllg_in = ''
+
+while cllg_in != 'n':
+    cllg_in = str(input("Do you have another college you want data about? (y or n)"))
+    if cllg_in == 'y':
+        college2 = str(input("What is the name of the college"))
+        print(college2)
+        cllg_namedf = dbr.loc[college2, :]
+        cllg_namedf = cllg_namedf.loc[{"UNITID", "CITY", "CONTROL", "CCBASIC", "CCUGPROF", "ADM_RATE_ALL", "SATVR75", "SATMT75", "SATMTMID", "SAT_AVG_ALL", "UGDS", "UGDS_WHITE", "UGDS_ASIAN", "TUITIONFEE_IN", "TUITIONFEE_OUT"}]
+        cllg_namedb = pandas.merge(cllg_namedb, cllg_namedf, left_index=True, right_index=True)
+    else:
+        cllg_namedb.to_excel("cllg.xlsx")
 
 
 
